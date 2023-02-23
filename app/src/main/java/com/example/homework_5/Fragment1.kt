aa85dfdbb87e2290fa2a7fa443c93ec4d92b1a79
koster06 <46241269+koster06@users.noreply.github.com>
2023-02-23 16:12:22 +0300

    add binding in Fragment1

---
+++ app/src/main/java/com/example/homework_5/Fragment1.kt
@@ -0,0 +1,27 @@
+package com.example.homework_5
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homework_5.databinding.Fragment1Binding
+
+
+class Fragment1 : Fragment() {
+
+    lateinit var binding: Fragment1Binding //add binding in Fragment1
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = Fragment1Binding.inflate(inflater)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = Fragment1()
+    }
+}
