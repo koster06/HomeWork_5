2c2d4e49bcffdc4a75959cf1a713f7bfa337db52
koster06 <46241269+koster06@users.noreply.github.com>
2023-02-23 16:30:36 +0300

    overrode all functions

--- app/src/main/java/com/example/homework_5/Fragment1.kt
+++ app/src/main/java/com/example/homework_5/Fragment1.kt
@@ -1,6 +1,7 @@
 package com.example.homework_5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +11,69 @@ import com.example.homework_5.databinding.Fragment1Binding
 
 class Fragment1 : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("test", "onCreate")
+    }
+
     lateinit var binding: Fragment1Binding //add binding in Fragment1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        Log.d("test", "onCreateView")
+
         binding = Fragment1Binding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("test", "onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("test", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("test", "onResume")
+    }
+
+    override fun onStop() {
+        Log.d("test", "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d("test", "onDestroyView")
+
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d("test", "onDestroy")
+
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d("test", "onDetach")
+
+        super.onDetach()
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = Fragment1()
     }
+
 }
