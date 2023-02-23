bbbb33add39364d4178734eeb6083db42bcdc9be
koster06 <46241269+koster06@users.noreply.github.com>
2023-02-23 16:44:50 +0300

    overrode all functions in Activity

--- app/src/main/java/com/example/homework_5/MainActivity.kt
+++ app/src/main/java/com/example/homework_5/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.homework_5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.homework_5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d("test", "Activity: onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("test", "Activity: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("test", "Activity: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("test", "Activity: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("test", "Activity: onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("test", "Activity: onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("test", "Activity: onDestroy")
+    }
+
 }
