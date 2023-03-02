36bafe9df0a0de49fc799f64b7a6141772c37125
koster06 <46241269+koster06@users.noreply.github.com>
2023-03-02 13:51:35 +0300

    Added Palette for imageView in MainActivity2

--- app/src/main/java/com/example/homework_5/MainActivity2.kt
+++ app/src/main/java/com/example/homework_5/MainActivity2.kt
@@ -1,36 +1,55 @@
 package com.example.homework_5
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
 import com.example.homework_5.databinding.ActivityMain2Binding
+
 
 class MainActivity2 : AppCompatActivity() {
 
     lateinit var binding: ActivityMain2Binding
-//    private var userIntent: ActivityResultLauncher<Intent>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-//        userIntent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//            if (it.resultCode == RESULT_OK) {
-//                it.data?.getSerializableExtra("user") as User
-//            }
-//        }
 
         val user = this.getIntent().getSerializableExtra("user") as User
-            with(binding){
-                textName.text = user.name
-                textName2.text= user.secName
-                textPhone.text = user.phone
-                textAge.text= user.age
-                textBirthday.text = user.birthday
-                imageView.setImageResource(user.id)
-            }
+        with(binding) {
+            Log.d("test", "In user create")
+            textAll.text = "${user.name}\n ${user.secName}\n ${user.phone}\n ${user.age}\n ${user.birthday}"
+            imageView.setImageResource(user.id)
+            val bm = (binding.imageView.drawable as BitmapDrawable).bitmap
+            setImageViewColor(bm)
+
+        }
+
     }
+    fun setImageViewColor(bitmap: Bitmap) {
+        // Generate the palette and get the vibrant swatch
+        val vibrantSwatch = createPaletteSync(bitmap).darkMutedSwatch
+
+        // Set the toolbar background and text colors.
+        // Fall back to default colors if the vibrant swatch is not available.
+        with(binding.imageView) {
+            setBackgroundColor(vibrantSwatch?.rgb ?:
+                ContextCompat.getColor(context, R.color.white))
+        }
+    }
+    private fun createPaletteSync(bitmap: Bitmap): Palette = Palette.from(bitmap).generate()
 
 
 }
+
+
