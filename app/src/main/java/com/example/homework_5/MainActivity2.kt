7b91fb40f4e4331e9aaa6ac9eef68e03d0bbee62
koster06 <46241269+koster06@users.noreply.github.com>
2023-03-23 17:36:02 +0300

    Fixed gradle - it was difficult

--- app/src/main/java/com/example/homework_5/MainActivity2.kt
+++ app/src/main/java/com/example/homework_5/MainActivity2.kt
@@ -1,12 +1,8 @@
 package com.example.homework_5
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.homework_5.databinding.ActivityMain2Binding
 
@@ -29,26 +25,7 @@ class MainActivity2 : AppCompatActivity() {
         userList?.forEach {
             adapterNextView.addNextUser(it)
         }
-//        val bm = (binding.imageView.drawable as BitmapDrawable).bitmap
-//        setImageViewColor(bm)
-
-
-
     }
-
-    fun setImageViewColor(bitmap: Bitmap) {
-        // Generate the palette and get the vibrant swatch
-        val vibrantSwatch = createPaletteSync(bitmap).darkMutedSwatch
-
-        // Set the toolbar background and text colors.
-        // Fall back to default colors if the vibrant swatch is not available.
-//        with(binding.imageView) {
-//            setBackgroundColor(vibrantSwatch?.rgb ?:
-//                ContextCompat.getColor(context, R.color.white))
-            //}
-    }
-    private fun createPaletteSync(bitmap: Bitmap): Palette = Palette.from(bitmap).generate()
-
 }
 
 
