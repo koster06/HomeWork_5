ee2909a4c28fd1e457954d9fe6c9ab3446b2daf8
koster06 <46241269+koster06@users.noreply.github.com>
2023-05-21 15:06:03 +0300

    18.1

--- app/src/main/java/com/example/homework_5/MainActivity.kt
+++ app/src/main/java/com/example/homework_5/MainActivity.kt
@@ -1,122 +1,18 @@
 package com.example.homework_5
 
-import adapter.UserAdapter
-import android.annotation.SuppressLint
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
-import androidx.work.WorkRequest
-import applications.appModule
 import com.example.homework_5.databinding.ActivityMain3Binding
-import com.google.firebase.perf.metrics.AddTrace
-import org.koin.android.ext.android.inject
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-import viewmodels.UserViewModel
-import viewmodels.UserViewModelFactory
 
-class MainActivity : AppCompatActivity(), UserAdapter.OnItemClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain3Binding
-    private lateinit var userAdapter: UserAdapter
-    private val viewModelFactory: UserViewModelFactory by inject()
 
-    @SuppressLint("SetTextI18n")
-    @AddTrace(name = "onCreateTrace", enabled = true)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startKoin {
-            androidContext(this@MainActivity)
-            modules(appModule)
-        }
-
-        createAlarm(this, 10000)
-
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.UNMETERED) // Wi-Fi подключение
-            .setRequiresBatteryNotLow(true) // Заряд батареи не низкий
-            .build()
-
-        val workRequest: WorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
-            .setConstraints(constraints)
-            .build()
-
-        WorkManager.getInstance(this).enqueue(workRequest)
-
-        userAdapter = UserAdapter.getInstance(this)
-        binding.recyclerView.adapter = userAdapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val viewModel = ViewModelProvider(this, viewModelFactory)[UserViewModel::class.java]
-        viewModel.users.observe(this) { users ->
-            userAdapter.setItems(users)
-        }
-
-        binding.floatingActionButton.setOnClickListener {
-            addUser()
-        }
-    }
-
-    private fun addUser() {
-        val fragment = FragmentNewUser()
-        supportFragmentManager.beginTransaction().apply {
-            setCustomAnimations(
-                R.anim.slide_in_left,
-                R.anim.slide_out_right,
-                R.anim.fade_in,
-                R.anim.fade_out
-            )
-                .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-
-    @AddTrace(name = "onItemClick", enabled = true)
-    override fun onItemClick(userId: Int) {
-        val fragment = FragmentUserDetails()
-        val args = Bundle()
-        args.putInt("userId", userId)
-        fragment.arguments = args
-        supportFragmentManager.beginTransaction().apply {
-            setCustomAnimations(
-                R.anim.slide_in_left,
-                R.anim.slide_out_right,
-                R.anim.fade_in,
-                R.anim.fade_out
-            )
-                .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-
-    private fun createAlarm(context: Context, delayMillis: Long) {
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val alarmIntent = Intent(context, AlarmReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            0,
-            alarmIntent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-
-        val triggerTime = SystemClock.elapsedRealtime() + delayMillis
-
-        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, pendingIntent)
     }
 }
 
---
+++ app/src/main/java/widgets/MyWidget.kt
@@ -0,0 +1,52 @@
+package widgets
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.util.Log
+import android.widget.RemoteViews
+import com.example.homework_5.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class MyWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        Log.d(MYWIDGET, "onUpdate called")
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        Log.d(MYWIDGET, "onEnabled called")
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        Log.d(MYWIDGET, "onDisabled called")
+        // Enter relevant functionality for when the last widget is disabled
+    }
+    companion object {
+        const val MYWIDGET = "MyWidget"
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.my_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
